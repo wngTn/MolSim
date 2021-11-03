b23890c7e3ecb34c55538de647a76c2bfbde3862
sev <severin.schmidmeier@tum.de>
2021-11-03 14:39:14 +0100

    added help message

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -24,17 +24,25 @@ static double start_time = 0;
 static double end_time = 1000;
 static double delta_t = 0.014;
 
+
 // default dimension
 static int DIM = 3;
 
 
 int main(int argc, char *argv[]) {
     std::cout<<"Hello from MolSim for PSE!"<<std::endl;
+    const std::string help = "Usage: ./MolSim [-i <input_file>] [-e <end_time>] [-d <delta_t>]\n"
+                             "\twhen leaving out -e/-d default values are used (1000/0.014)\n"
+                             "\twhen leaving out -i a random input file is generated automatically \n"
+                             "\tcall with flag -h to display this message\n";
 
     int opt;
     std::string filename;
-    while((opt = getopt(argc, argv, "i:e:d:")) != -1){
+    while((opt = getopt(argc, argv, "hi:e:d:")) != -1){
         switch(opt){
+            case 'h':
+                std::cout << help;
+                return 0;
             case 'i':
                 filename = optarg;
                 break;
@@ -45,7 +53,8 @@ int main(int argc, char *argv[]) {
                 delta_t = std::stod(optarg);
                 break;
             case '?':
-                std::cerr <<"Unknown option: " << optopt << "\n";
+                std::cerr << "Unknown option: " << optopt << "\n";
+                std::cerr << help;
                 break;
             default:
                 break;
@@ -56,6 +65,7 @@ int main(int argc, char *argv[]) {
     // if no input file has been specified, generate random input using python script
     if(filename.empty()){
         std::cout << "Generating random input (this needs python to be installed)\n";
+        // maybe change particle amount
         std::system("python ../generate_input.py -n 50 -o input.txt");
         filename = "input.txt";
     }
